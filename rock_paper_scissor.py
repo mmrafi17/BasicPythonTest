d33a3eb01723673e51cbae01945e6f380eb81d5b
mmrafi17 <rafimrf17@gmail.com>
2024-12-19 18:41:09 +0700

    updating rock_paper_scissor.py

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -6,19 +6,25 @@
 
 import random
 
-while True:
-    emojis = {'r': '🪨', 'p': '📃', 's': '✂️'}
-    choices = ('r','p','s')
-    user_choice = input('Rock, Paper, or Scissors? (r/p/s): ').lower()
+emojis = {'r': '🪨', 'p': '📃', 's': '✂️'}
+choices = ('r','p','s')
 
-    if user_choice not in choices:
-        print('Invalid Choice!')
-        continue
+def get_user_choice():
+
+    while True:
+        user_choice = input('Rock, Paper, or Scissors? (r/p/s): ').lower()
+        if user_choice in choices:
+            return user_choice
+        else:
+            print('Invalid Choice!')
+            continue
+        
     
-    computer_choice = random.choice(choices)    
+def display_choice(user_choice, computer_choice):
     print(f'You choose {emojis[user_choice]}')
     print(f'Computer Choice {emojis[computer_choice]}')
 
+def determine_winning(user_choice, computer_choice):
     if user_choice == computer_choice:
         print('Tie!')
     elif (
@@ -28,11 +34,19 @@ while True:
         print('You Won')
     else:
         print('You Lose')
+        
+def play_game():
+    while True:
+        computer_choice = random.choice(choices)    
+        
+        user_choice = get_user_choice()
+        display_choice(user_choice, computer_choice)
+        determine_winning(user_choice, computer_choice)
+        
 
-    should_continue = input("Do you want to continue? y/n: ")
+        should_continue = input("Do you want to continue? y/n: ")
 
-    if should_continue == 'n':
-        break
+        if should_continue == 'n':
+            break
 
-
-
+play_game()
